a4fcd23755c60d50cc6350ec8364e9d8af2f0b5d
sumitra101 <ksumitra69@hotmail.com>
2017-11-01 20:10:26 +0545

    Access data through database

--- asp.netProject/asp.netProject/Controllers/EmployeeController.cs
+++ asp.netProject/asp.netProject/Controllers/EmployeeController.cs
@@ -10,16 +10,23 @@ namespace asp.netProject.Controllers
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public ActionResult Details()
+        public ActionResult Details(int id)
         {
-            Employee employee = new Employee()
-            {
-                EmployeeId = 101,
-                Name = "Ram",
-                Gender = "Male",
-                City = "Pokhara"
-            };
+            EmployeeContext employeeContextKoObject = new EmployeeContext();
+            Employee employee = new Employee();
+            employee = employeeContextKoObject.abcd.SqlQuery("Select * from tblEmployee where EmployeeId = @p0", id).FirstOrDefault();
+            //Employee employee = employeeContext.Employees.Single(emp => emp.EmployeeId == id);
             return View(employee);
+        }
+
+        public ActionResult see()
+        {
+            Employee nayaemp = new Employee();
+            nayaemp.EmployeeId = 15;
+            nayaemp.Gender = "male";
+            nayaemp.Name = "testname";
+            nayaemp.City = "pkr";
+            return View(nayaemp);
         }
     }
 }
