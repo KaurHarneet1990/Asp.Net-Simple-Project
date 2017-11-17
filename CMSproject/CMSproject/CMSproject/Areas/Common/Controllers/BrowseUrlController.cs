1b317dd0ecb7faaab5c94b315e1d6d7179379929
sumitra101 <ksumitra69@hotmail.com>
2017-11-17 21:35:00 +0545

    Customize sorting

--- CMSproject/CMSproject/CMSproject/Areas/Common/Controllers/BrowseUrlController.cs
+++ CMSproject/CMSproject/CMSproject/Areas/Common/Controllers/BrowseUrlController.cs
@@ -15,9 +15,64 @@ namespace CMSproject.Areas.Common.Controllers
             objBll = new UrlBll();
         }
         // GET: Common/BrowseUrl
-        public ActionResult Index()
+        public ActionResult Index(string SortOrder,string SortBy)
         {
-            var url = objBll.GetAll().Where(x=>x.IsApproved=="A").ToList();
+            ViewBag.SortOrder = SortOrder;
+            ViewBag.SortBy = SortBy;
+            var url = objBll.GetAll().Where(x=>x.IsApproved=="A");
+            switch(SoryBy)
+            {
+                case"Title":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            url = url.OrderBy(x => x.UrlITitle).ToList();
+                            break;
+                        case "Desc":
+                            url = url.OrderByDescending(x => x.UrlITitle).ToList();
+                            break;
+                    }
+                    break;
+                case "Category":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            url = url.OrderBy(x => x.tbl_Category.CategoryName).ToList();
+                            break;
+                        case "Desc":
+                            url = url.OrderByDescending(x => x.tbl_Category.CategoryName).ToList();
+                            break;
+                    }
+                    break;
+                case "Url":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            url = url.OrderBy(x => x.Url).ToList();
+                            break;
+                        case "Desc":
+                            url = url.OrderByDescending(x => x.Url).ToList();
+                            break;
+                    }
+                    break;
+                case "UrlDesc":
+                    switch (SortOrder)
+                    {
+                        case "Asc":
+                            url = url.OrderBy(x => x.UrlDesc).ToList();
+                            break;
+                        case "Desc":
+                            url = url.OrderByDescending(x => x.UrlDesc).ToList();
+                            break;
+                    }
+                    break;
+                default:
+                    url = url.OrderBy(x => x.UrlITitle).ToList();
+                    break;
+
+
+            }
+            
             return View(url);
         }
     }
