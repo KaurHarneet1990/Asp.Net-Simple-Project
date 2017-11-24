1b860a1ddbaf3b5b8ef73b519a49db33de695f35
sumitra101 <ksumitra69@hotmail.com>
2017-11-24 21:47:33 +0545

    Custom Validation

--- CMSproject/BOL/tbl_Url.cs
+++ CMSproject/BOL/tbl_Url.cs
@@ -9,12 +9,26 @@ namespace BOL
 {
     public partial class tbl_Url
     {
+        public class UniqueUrlAttribute : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                CMSdbContext db = new CMSdbContext();
+                string urlValue = value.ToString();
+                int count = db.TableUrl.Where(x => x.Url == urlValue).ToList().Count();
+                if (count != 0)
+                    return new ValidationResult("Url Already Exits.");
+                return ValidationResult.Success;
+            }
+        }
+
         [Key]
         public int UrlId { get; set; }
         [Required]
         public string UrlITitle{ get; set; }
         [Required]
         [Url]
+        [UniqueUrl]
         public string Url { get; set; }
         [Required]
         public string UrlDesc { get; set; }
