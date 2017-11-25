ee14e3068249a3cd63d36183d09aec3c2cec857d
sumitra101 <ksumitra69@hotmail.com>
2017-11-25 20:59:13 +0545

    Adding Base Class in Business logic layer(BLL)

---
+++ CMSproject/BLL/AdminBll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class AdminBll
+    {
+        public CategoryBll categoryBll { get; set; }
+        public UserBll userBll { get; set; }
+        public UrlBll urlBll { get; set; }
+
+        public AdminBll()
+        {
+            categoryBll = new CategoryBll();
+            userBll = new UserBll();
+            urlBll = new UrlBll();
+        }
+    }
+}
